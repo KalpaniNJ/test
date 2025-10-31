e9354c62877d7f503ef2513ff90eac92a176adff
KalpaniNJ <kalpanijayamini@gmail.com>
2025-10-31 14:32:04 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -2,21 +2,22 @@ import ee
 
 
 AOI_OPTIONS = {
-    "MahaKanadarawa Water Influence Zone": "projects/rice-mapping-472904/assets/mk_influence_zone",
-    "MahaKanadarawa Irrigable Area": "projects/rice-mapping-472904/assets/mk_Irrigable_Area"
+    # "MahaKanadarawa Water Influence Zone": "projects/rice-mapping-472904/assets/mk_influence_zone",
+    # "MahaKanadarawa Irrigable Area": "projects/rice-mapping-472904/assets/mk_Irrigable_Area"
+    "Walawa Irrigation Scheme": "projects/ricemapping-475407/assets/UWIS_aoi"
 }
 
 ASSETS = {
-    "points": "projects/rice-mapping-472904/assets/SamplePtsMahaKanadarawa",
-    "roads": "projects/rice-mapping-472904/assets/mkRoads",
-    "water": "projects/rice-mapping-472904/assets/mkTanks",
+    "points": "projects/ricemapping-475407/assets/UWIS_pts",
+    "roads": "projects/ricemapping-475407/assets/UWIS_roads",
+    "water": "projects/ricemapping-475407/assets/UWIS_water",
 }
 
 def load_assets():
     return {
         # AOIs
-        "WIZ": ee.FeatureCollection(AOI_OPTIONS["MahaKanadarawa Water Influence Zone"]),
-        "IA": ee.FeatureCollection(AOI_OPTIONS["MahaKanadarawa Irrigable Area"]),
+        "WIS": ee.FeatureCollection(AOI_OPTIONS["Walawa Irrigation Scheme"])
+        # "IA": ee.FeatureCollection(AOI_OPTIONS["MahaKanadarawa Irrigable Area"]),
 
         # Supporting layers
         "points": ee.FeatureCollection(ASSETS["points"]),
