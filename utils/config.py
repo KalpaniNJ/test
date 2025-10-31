5b7f5e0b3923b49dd551b92c5a9e4d685bbf6b35
KalpaniNJ <kalpanijayamini@gmail.com>
2025-10-31 14:44:29 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -2,8 +2,6 @@ import ee
 
 
 AOI_OPTIONS = {
-    # "MahaKanadarawa Water Influence Zone": "projects/rice-mapping-472904/assets/mk_influence_zone",
-    # "MahaKanadarawa Irrigable Area": "projects/rice-mapping-472904/assets/mk_Irrigable_Area",
     "Walawa Irrigation Scheme": "projects/ricemapping-475407/assets/UWIS_aoi"
 }
 
